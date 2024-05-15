d5198f32fea6e7a1e6d8eeddd37f644068979f8d
Michael Carey <Michael@AGameEmpowerment.com>
2024-05-15 20:28:01 +0000

    feat: Add Home page and routes components
    Add new files for the Home page and Routes component in the InterviewChallenge.Web project.

--- run_tests.ps1
+++ run_tests.ps1
@@ -2,8 +2,8 @@
 [string] $loggerArgument = '"trx;logfilename=Test_Results.' + (Get-Date).ToString("yyyyMMdd.HHmmss") + '.trx"'
 
 # Run the automated tests
-Set-Location .\Src\Tests\InterviewChallenge.Tests\
+Set-Location .\Src\InterviewChallenge.Tests\
 dotnet build --configuration Debug --source .\InterviewChallenge.Tests.sln
 dotnet test .\InterviewChallenge.Tests.sln --logger $loggerArgument #--collect:"Code Coverage"
 
-Set-Location ..\..\..
+Set-Location ..\..
