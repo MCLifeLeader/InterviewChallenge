77f0aada96b1dfaa8367594fdf2927d536420987
Michael Carey <Michael@AGameEmpowerment.com>
2024-05-15 23:20:32 +0000

    Update devcontainer.json to use lowercase file name for post_create.ps1

--- post_create.ps1
+++ post_create.ps1
@@ -1,22 +1,26 @@
 # Setup Development Environment DevContainer
 Write-Host "Post Create Commands for Environment..."
 
+dotnet workload update
+dotnet workload install aspire
+dotnet tool install -g Microsoft.Web.LibraryManager.Cli
+
 Set-Location ./Src/
 
 Set-Location ./InterviewChallenge.ApiService
 dotnet restore InterviewChallenge.ApiService.csproj
 
-Set-Location ../../InterviewChallenge.AppHost
+Set-Location ../InterviewChallenge.AppHost
 dotnet restore InterviewChallenge.AppHost.csproj
 
-Set-Location ../../InterviewChallenge.ServiceDefaults
+Set-Location ../InterviewChallenge.ServiceDefaults
 dotnet restore InterviewChallenge.ServiceDefaults.csproj
 
-Set-Location ../../InterviewChallenge.Web
+Set-Location ../InterviewChallenge.Web
 dotnet restore InterviewChallenge.Web.csproj
 libman restore
 
-Set-Location ../../InterviewChallenge.Tests
+Set-Location ../InterviewChallenge.Tests
 dotnet restore InterviewChallenge.Tests.csproj
 
 Set-Location ../..
