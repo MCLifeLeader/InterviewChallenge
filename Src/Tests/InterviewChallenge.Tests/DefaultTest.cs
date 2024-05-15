c762a25eaf2bf07d3c963a0816ef793a325664c6
Michael Carey <Michael@AGameEmpowerment.com>
2024-05-15 20:05:34 +0000

    feat: Add InterviewChallenge.Tests project
    The commit adds the InterviewChallenge.Tests project to the solution.

---
+++ Src/Tests/InterviewChallenge.Tests/DefaultTest.cs
@@ -0,0 +1,25 @@
+namespace InterviewChallenge.Tests;
+
+/// <summary>
+/// This is the baseline default test and should remain in the project and unchanged.
+/// </summary>
+[TestFixture]
+public class DefaultTest
+{
+    /// <summary>
+    /// Setup method for the test fixture.
+    /// </summary>
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    /// <summary>
+    /// Test method to test the test framework.
+    /// </summary>
+    [Test]
+    public void Test_Pass()
+    {
+        Assert.Pass();
+    }
+}
--- run_tests.ps1
+++ run_tests.ps1
@@ -1,9 +1,9 @@
 #Setup dotnet test run argument
-[string] $loggerArgument = '\"trx;logfilename=Test_Results.' + (Get-Date).ToString("yyyyMMdd.HHmmss") + '.trx\"'
+[string] $loggerArgument = '"trx;logfilename=Test_Results.' + (Get-Date).ToString("yyyyMMdd.HHmmss") + '.trx"'
 
 # Run the automated tests
 Set-Location .\Src\Tests\InterviewChallenge.Tests\
-dotnet build --configuration Debug --source .\InterviewChallenge.Tests.sln --source ..\..\..\Nuget\nuget.config
+dotnet build --configuration Debug --source .\InterviewChallenge.Tests.sln
 dotnet test .\InterviewChallenge.Tests.sln --logger $loggerArgument #--collect:"Code Coverage"
 
 Set-Location ..\..\..
